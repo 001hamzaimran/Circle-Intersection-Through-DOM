6e9aadadb1f9817cf61a63809ae5c324aeafeaa2
001hamzaimran <syed.hamza2004@gmail.com>
2024-04-30 05:44:47 +0500

    Completed the Task Question

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ document.addEventListener('click', (e) => {
 
     let Circle = document.createElement("div");
     Circle.className = "circle";
-    Circle.style.width = circleRadius + "px";
-    Circle.style.height = circleRadius+ "px"
-    Circle.style.top = y-(circleRadius/2) + "px";
-    Circle.style.left = x-(circleRadius/2) + "px";
+    Circle.style.width = `${circleRadius}px`;
+    Circle.style.height = `${circleRadius}px`;
+    Circle.style.top = y - (circleRadius / 2) + "px";
+    Circle.style.left = x - (circleRadius / 2) + "px";
 
     document.body.append(Circle);
 
-    circleCoOrdinate.push(x,y);
+    circleCoOrdinate.push({ x, y, r: circleRadius } );
     console.log(circleCoOrdinate);
 
 
@@ -26,6 +26,30 @@ document.addEventListener('click', (e) => {
     if (countCircle.length > 2) {
         countCircle.forEach(element => {
             document.body.removeChild(element)
+
         });
+        FindIfCircleIntersecting()
+        circleCoOrdinate = []
+    }
+
+    function FindIfCircleIntersecting() {
+        for (let i = 0; i < circleCoOrdinate.length; i++) {
+            for (let j = i + 1; j < circleCoOrdinate.length; j++) {
+                let x1 = circleCoOrdinate[i].x;
+                let y1 = circleCoOrdinate[i].y;
+                let r1 = circleCoOrdinate[i].r;
+                let x2 = circleCoOrdinate[j].x;
+                let y2 = circleCoOrdinate[j].y;
+                let r2 = circleCoOrdinate[j].r;
+                
+                let distance = Math.hypot(x1 - x2, y1 - y2);
+                
+                if (distance < r1 + r2) {
+                    alert("Circles intersect!");
+                    return;
+                }
+            }
+        }
+        alert("No Intersection detected")
     }
 })
